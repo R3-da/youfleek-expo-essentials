c7f850ed8ff0ed7bcf60111fa0c58ab36eabc08e
R3-da <haddan.reda.pers@gmail.com>
2023-08-30 16:37:29 +0100

    fix packages and manifest deprecated error, still have some errors stopping the login screen from showing

--- client/app/api/client.js
+++ client/app/api/client.js
@@ -1,10 +1,9 @@
 import { create } from "apisauce";
 import authStorage from "../auth/storage";
 import Constants from "expo-constants";
-const { manifest } = Constants;
 
 const apiClient = create({
-  baseURL: `http://${manifest.debuggerHost.split(':').shift()}:8000`,
+  baseURL: `http://${Constants.expoGoConfig.debuggerHost.split(':').shift()}:8000`,
 });
 
 apiClient.addAsyncRequestTransform(async (request) => {
